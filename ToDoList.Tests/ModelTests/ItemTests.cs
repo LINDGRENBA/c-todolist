cfb5cf6f298c8e4e89f59c595be2dc43af2050f1
blacchoy@gmail.com <blacchoy@gmail.com>
2020-07-15 13:21:27 -0700

    pass 4th test > > Co-authored-by: Brittany Lindgren <brittdevelopsit@gmail.com>

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -1,13 +1,18 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ToDoList.Models;
 using System.Collections.Generic;
+using System;
 
 namespace ToDoList.Tests     
 {
   [TestClass]
-  public class ItemTests 
+  public class ItemTests :IDisposable
   {
 
+  public void Dispose()
+  {
+    Item.ClearAll();
+  }
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item()
     {
@@ -42,7 +47,6 @@ namespace ToDoList.Tests
       List<Item> newList = new List<Item> { };
       List<Item> result = Item.GetAll();
       CollectionAssert.AreEqual(newList, result);
-
     }
 
   }
