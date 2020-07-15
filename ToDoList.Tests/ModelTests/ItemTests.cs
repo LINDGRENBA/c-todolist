70ddff02fd20e77e4d8e2f6332906147457f7f3b
blacchoy@gmail.com <blacchoy@gmail.com>
2020-07-15 14:23:49 -0700

    fail for purpose of seeing persisting items
    Co-authored-by: Brittany Lindgren <brittdevelopsit@gmail.com>

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -9,10 +9,11 @@ namespace ToDoList.Tests
   public class ItemTests :IDisposable
   {
 
-  public void Dispose()
-  {
-    Item.ClearAll();
-  }
+    public void Dispose()
+    {
+      // Item.ClearAll();
+    }
+
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item()
     {
@@ -46,6 +47,28 @@ namespace ToDoList.Tests
     {
       List<Item> newList = new List<Item> { };
       List<Item> result = Item.GetAll();
+      foreach (Item thisItem in result)
+      {
+        Console.WriteLine("Output from empty list GetAll test: " + thisItem.Description);
+      }
+      CollectionAssert.AreEqual(newList, result);
+    } 
+
+    [TestMethod]
+    public void GetAll_ReturnsItems_ItemList()
+    {
+      string description01 = "Brush the cat";
+      string description02 = "Clean the litterbox";
+      Item newItem1 = new Item(description01);
+      Item newItem2 = new Item(description02);
+      List<Item> newList = new List<Item> { newItem1, newItem2 };
+
+      List<Item> result = Item.GetAll();
+      foreach(Item thisItem in result)
+      {
+        Console.WriteLine("Output from second GetAll test: " + thisItem.Description);
+      }
+
       CollectionAssert.AreEqual(newList, result);
     }
 
